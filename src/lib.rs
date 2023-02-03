28dc91a71eac37c3c66860fdb51ffdba8c398c8b
nmandrus1 <nmandrus1@gmail.com>
2023-02-02 19:06:07 -0500

    Setting up WebSocket Connection endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, Error, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
+// basic health check end_point
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
+}
+
+// entry point to the webscoket connection
+async fn calendar_route() -> Result<HttpResponse, Error> {
+    // start the web socket server here
+    todo!()
 }
 
 // return an instance of our server
