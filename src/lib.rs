ce7628353e7dba25ad9386d716b945d6bf302adb
nmandrus1 <nmandrus1@gmail.com>
2023-02-10 20:41:15 -0500

    Websocket server and session are working

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-use actix_web::{dev::Server, web, App, Error, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
+use actix::{Actor, Addr, StreamHandler};
+use actix_web::{dev::Server, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
+use session::WsCalSession;
+use std::{net::TcpListener, time::Instant};
+
+mod server;
+mod session;
 
 // basic health check end_point
 async fn health_check() -> impl Responder {
@@ -7,16 +13,37 @@ async fn health_check() -> impl Responder {
 }
 
 // entry point to the webscoket connection
-async fn calendar_route() -> Result<HttpResponse, Error> {
+async fn ws_route(
+    req: HttpRequest,
+    stream: web::Payload,
+    srv: web::Data<Addr<server::CalServer>>,
+) -> Result<HttpResponse, Error> {
     // start the web socket server here
-    todo!()
+    println!("Knock Knock");
+    ws::start(
+        session::WsCalSession {
+            id: 0,
+            hb: Instant::now(),
+            addr: srv.get_ref().clone(),
+        },
+        &req,
+        stream,
+    )
 }
 
 // return an instance of our server
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    // start calendar server
+    let server = server::CalServer::new().start();
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(server.clone()))
+            .route("/health_check", web::get().to(health_check))
+            .route("/ws", web::get().to(ws_route))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
