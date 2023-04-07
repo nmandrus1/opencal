12200a33e3c625b20ba22acaff86a6e67a8e11b6
Ethan Tuttle <ethantut@gmail.com>
2023-04-07 17:21:10 -0400

    calendar rows done, need styling

--- frontend/js/web.js
+++ frontend/js/web.js
@@ -1,18 +1,32 @@
 // --------------------------------------------------------
 //              collapsible tag groups
 window.onload = function() {
-    Array.from(document.getElementsByClassName("collapsible")).forEach(button => {
-        button.addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-              content.style.maxHeight = null;
-            } else {
-              content.style.maxHeight = content.scrollHeight + "px";
-            }
-          });
-        button.click();
+  Array.from(document.getElementsByClassName("collapsible")).forEach(button => {
+      button.addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+        });
+      button.click();
+  });
+
+  var output = "";
+  var days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+  var hours = ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am", "9am", "10am", "11am", 
+                "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
+  hours.forEach(function(hour) {
+    output += "<tr id=\"" + hour + "-row\">";
+    output += "<td id=\"" + hour + "\">" + hour + "</td>";
+    days.forEach(function(day) {
+      output += "<td id=\"" + hour + "-" + day + "\"></td>";
     });
+    output += "</tr>";
+  });
+  $("#calendarTable tbody").after(output);
 }
 
 // --------------------------------------------------------
