175857e7ad0fe94a11750a02d89b49b10db57b50
kardotha <mimeng2518@gmail.com>
2023-04-25 19:34:29 -0400

    email client settings

--- src/event.rs
+++ src/event.rs
@@ -49,3 +49,40 @@ impl TryFrom<String> for Environment {
         }
     }
 }
+
+pub struct EmailClient {
+    http_client: Client,
+    base_url: String,
+    sender: SubscriberEmail,
+    authorization_token: String,
+}
+impl EmailClient {
+    pub fn new(base_url: String, sender: SubscriberEmail, authorization_token: String) -> Self {
+        Self {
+            http_client: Client::new(),
+            base_url,
+            sender,
+            authorization_token,
+        }
+    }
+}
+impl EmailClient {
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        let url = format!("{}/email", self.base_url);
+        let request_body = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_body: html_content.to_owned(),
+            text_body: text_content.to_owned(),
+        };
+        let builder = self.http_client.post(&url).json(&request_body);
+        Ok(())
+    }
+}
